0a37fe0587a37152233fc7a81ece78f2b6e0a5e0
Ethan Benckwitz <60863350+ebenckwitz@users.noreply.github.com>
2020-11-17 17:23:56 -0700

    Minimum Cost to Move Chips to The Same Position

---
+++ minCostToMoveChips.java
@@ -0,0 +1,20 @@
+/*
+We have n chips, where the position of the ith chip is position[i].
+
+We need to move all the chips to the same position. In one step, we can change the position of the ith chip from position[i] to:
+
+position[i] + 2 or position[i] - 2 with cost = 0.
+position[i] + 1 or position[i] - 1 with cost = 1.
+Return the minimum cost needed to move all the chips to the same position.
+*/
+
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int odd = 0, even = 0;
+        for(int i : position) {
+            odd += (i % 2 == 0) ? 1 : 0;
+            even += (i % 2 == 0) ? 0 : 1;
+        }
+        return Math.min(odd, even);
+    }
+}
