160071bcc98113d242f95b929098bf36418eeb99
Ethan Benckwitz <60863350+ebenckwitz@users.noreply.github.com>
2021-01-05 15:44:32 -0700

    Reverse Linked List

---
+++ reverseList.java
@@ -0,0 +1,34 @@
+/*
+Reverse a singly linked list.
+
+Example:
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        
+        while(curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            head = prev;
+        }
+        return head;
+        
+    }
+}
